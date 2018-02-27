77b7b41236d932be72d90b1e81a76ebb24df7ab7
Mikkel Sandberg <mikkel.h.sandberg@gmail.com>
2018-02-27 11:48:29 -0800

    Adding components to app.js; fixing bug on SearchBar

--- ravenous/src/components/SearchBar/SearchBar.js
+++ ravenous/src/components/SearchBar/SearchBar.js
@@ -12,7 +12,7 @@ class SearchBar extends React.Component {
 		return Object.keys(sortByOptions).map(sortByOption => {
 			let sortByOptionValue = sortByOptions[sortByOption];
 
-			<li key={sortByOptionValue}>{sortByOption}</li>
+			return <li key={sortByOptionValue}>{sortByOption}</li>;
 		});
 	}
 
@@ -21,7 +21,7 @@ class SearchBar extends React.Component {
 			<div className="SearchBar">
 				<div className="SearchBar-sort-options">
 					<ul>
-						{this.renderSortByOptions}
+						{this.renderSortByOptions()}
 					</ul>
 				</div>
 				<div className="SearchBar-fields">
