906c284a1bb759d4adf7767adc751e9cecf1cb22
Mikkel Sandberg <mikkel.h.sandberg@gmail.com>
2018-03-06 11:23:26 -0800

    adding sorting feedback; getting search button ready

--- ravenous/src/components/SearchBar/SearchBar.js
+++ ravenous/src/components/SearchBar/SearchBar.js
@@ -8,11 +8,56 @@ const sortByOptions = {
 };
 
 class SearchBar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			term: '',
+			location: '',
+			sortBy: 'best_match'
+		};
+		this.handleTermChange = this.handleTermChange.bind(this);
+		this.handleLocationChange = this.handleLocationChange.bind(this);
+		this.handleSearch = this.handleSearch.bind(this);
+	}
+
+	getSortByClass(sortByOption) {
+		return this.state.sortBy === sortByOption ? 'active' : '';
+	}
+
+	handleSortByChange(sortByOption) {
+		this.setState({
+			sortBy: sortByOption
+		});
+	}
+
+	handleTermChange(e) {
+		this.setState({
+			term: e.target.value
+		});
+	}
+
+	handleLocationChange(e) {
+		this.setState({
+			location: e.target.value
+		});
+	}
+
+	handleSearch(e) {
+		e.preventDefault();
+		this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+	}
+
 	renderSortByOptions() {
 		return Object.keys(sortByOptions).map(sortByOption => {
 			let sortByOptionValue = sortByOptions[sortByOption];
 
-			return <li key={sortByOptionValue}>{sortByOption}</li>;
+			return (
+				<li
+					key={sortByOptionValue}
+					className={this.getSortByClass(sortByOptionValue)}
+					onClick={this.handleSortByChange.bind(this, sortByOptionValue)}>
+				{sortByOption}</li>
+			);
 		});
 	}
 
@@ -25,11 +70,15 @@ class SearchBar extends React.Component {
 					</ul>
 				</div>
 				<div className="SearchBar-fields">
-					<input placeholder="Search Businesses" />
-					<input placeholder="Where?" />
+					<input
+						placeholder="Search Businesses"
+						onChange={this.handleTermChange} />
+					<input
+						placeholder="Where?"
+						onChange={this.handleLocationChange} />
 				</div>
 				<div className="SearchBar-submit">
-					<a>Let's Go</a>
+					<a onClick={this.handleSearch}>Let's Go</a>
 				</div>
 			</div>
 		);
